7a54b6afa13e2ecdd24642fe855b12e37800755c
Adrysson <adrysson@doity.com.br>
2020-11-11 01:14:07 -0300

    Criando base do formulário de pesquisa

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,49 +1,45 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="q-ma-xl">
+    <div class="row justify-center">
+      <h1 class="text-h4">Pedidos</h1>
+    </div>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <div class="row justify-center">
+        <q-select
+          outlined
+          v-model="model"
+          :options="options"
+          label="Clientes"
+          class="q-ma-sm"
+        />
+        <q-input filled type="number" v-model="name" label="Valor" class="q-ma-sm" />
+        <q-input filled type="number" v-model="name" label="Até" class="q-ma-sm" />
+      </div>
+      <div class="row justify-center">
+        <q-btn label="Pesquisar" type="submit" color="primary" />
+        <q-btn
+          label="Limpar"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/CompositionComponent.vue'
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { ExampleComponent },
-  setup () {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1'
-      },
-      {
-        id: 2,
-        content: 'ct2'
-      },
-      {
-        id: 3,
-        content: 'ct3'
-      },
-      {
-        id: 4,
-        content: 'ct4'
-      },
-      {
-        id: 5,
-        content: 'ct5'
-      }
-    ])
-    const meta = ref<Meta>({
-      totalCount: 1200
-    })
-    return { todos, meta }
+  data () {
+    return {
+      clientes: []
+    }
+  },
+  mounted () {
   }
 })
 </script>
