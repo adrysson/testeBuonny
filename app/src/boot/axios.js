a9a62e40f14754c53c8e6a4c9e8f9a9fedef2cea
Adrysson <adrysson@doity.com.br>
2020-11-11 12:15:19 -0300

    Adicionando loading padrão para as requisições

--- app/src/boot/axios.js
+++ app/src/boot/axios.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { Loading } from 'quasar'
 
 axios.defaults.headers.common.Accept = 'application/json'
 axios.defaults.baseURL = 'http://localhost:8765/v1'
+
+// interceptors
+axios.interceptors.request.use((config) => {
+  Loading.show({
+    delay: 400
+  })
+  return config
+})
+
+axios.interceptors.response.use((response) => {
+  Loading.hide()
+  return response
+})
 
 Vue.prototype.$axios = axios
